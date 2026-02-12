968b4a2d9827df7d0cfcafd309088e8fd3f83336
matitooo <dagostinimattia0@gmail.com>
2026-02-12 17:03:00 +0100

    first commit

--- model_utils.py
+++ model_utils.py
@@ -1,15 +1,22 @@
 from torch.nn import CrossEntropyLoss
 import torch
+from tqdm import tqdm
 
 def train_model(model,optimizer,train_dataloader,val_dataloader,criterion,n_epochs):
-    for epoch in range(n_epochs):
+    for epoch in tqdm(range(n_epochs)):
         model.train()
         optimizer.zero_grad()
+        train_loss = 0 
+        total_train = 0 
         for images, labels in train_dataloader:
             out = model(images)
             loss = criterion(out,labels)
             loss.backward()
+                               
+            train_loss += loss.item() * images.size(0)
+            total_train += images.size(0)
             optimizer.step()
+        print(f"Epoch {epoch+1}/{n_epochs} | Train Loss: {train_loss/total_train:.4f}")    
         if epoch%10 == 0:
             model.eval()
             val_loss = 0
